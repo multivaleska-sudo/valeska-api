ad62e132bc5f30c49553de995ac8846d85d01273
LavenderEdit <gercermagden@gmail.com>
2026-06-16 18:50:17 -0500

    feat: add SyncWriteContext parameter to upsertClientes and upsertVehiculos methods in IMaestrosSyncRepository

--- src/sync/domain/ports/maestros-sync-repository.interface.ts
+++ src/sync/domain/ports/maestros-sync-repository.interface.ts
@@ -9,7 +9,7 @@ import {
 } from '../../../tramites/entities/maestros.entity';
 import { PerfilGestor } from '../../../tramites/entities/perfil-gestor.entity';
 import { MessageTemplate } from '../../../tramites/entities/plantillas.entity';
-import type { SyncPushResult } from '../sync-push-result';
+import type { SyncPushResult, SyncWriteContext } from '../sync-push-result';
 
 export const MAESTROS_SYNC_REPOSITORY_TOKEN = Symbol('IMaestrosSyncRepository');
 
@@ -18,8 +18,8 @@ export const MAESTROS_SYNC_REPOSITORY_TOKEN = Symbol('IMaestrosSyncRepository');
  */
 export interface IMaestrosSyncRepository {
     // --- MÉTODOS DE ESCRITURA POR LOTES (UPSERT) ---
-    upsertClientes(tx: EntityManager, clientes: Partial<Cliente>[]): Promise<SyncPushResult>;
-    upsertVehiculos(tx: EntityManager, vehiculos: Partial<Vehiculo>[]): Promise<SyncPushResult>;
+    upsertClientes(tx: EntityManager, clientes: Partial<Cliente>[], context?: SyncWriteContext): Promise<SyncPushResult>;
+    upsertVehiculos(tx: EntityManager, vehiculos: Partial<Vehiculo>[], context?: SyncWriteContext): Promise<SyncPushResult>;
     upsertEmpresasGestoras(tx: EntityManager, empresas: Partial<EmpresaGestora>[]): Promise<void>;
     upsertPlantillasDocumentos(tx: EntityManager, plantillas: Partial<PlantillaDocumento>[]): Promise<void>;
     upsertPresentantes(tx: EntityManager, presentantes: Partial<Presentante>[]): Promise<void>;
